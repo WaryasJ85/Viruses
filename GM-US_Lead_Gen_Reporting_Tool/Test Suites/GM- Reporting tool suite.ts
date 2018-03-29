39af065892810b8f3647f9412d325ae8075ff635
WaryasJ85 <jasonwaryas@gmail.com>
2018-03-29 14:04:20 -0400

    03292018_1404
    Updated some things

---
+++ GM-US_Lead_Gen_Reporting_Tool/Test Suites/GM- Reporting tool suite.ts
@@ -0,0 +1,13 @@
+<?xml version="1.0" encoding="UTF-8"?>
+<TestSuiteEntity>
+   <description>Just a start</description>
+   <name>GM- Reporting tool suite</name>
+   <tag></tag>
+   <isRerun>false</isRerun>
+   <mailRecipient>jwaryas@bluewatertech.com;</mailRecipient>
+   <numberOfRerun>0</numberOfRerun>
+   <pageLoadTimeout>30</pageLoadTimeout>
+   <pageLoadTimeoutDefault>true</pageLoadTimeoutDefault>
+   <rerunFailedTestCasesOnly>false</rerunFailedTestCasesOnly>
+   <testSuiteGuid>970114b9-bf42-442e-ae72-7660b0a0e74a</testSuiteGuid>
+</TestSuiteEntity>
