f0a3ace7c70cd9dd0f6bccbfd9b6cf2138c258d6
WaryasJ85 <jasonwaryas@gmail.com>
2018-03-29 15:22:39 -0400

    032918_1522

---
+++ GM-US_Lead_Gen_Reporting_Tool/Libs/TempTestCase1522349607910.groovy
@@ -0,0 +1,25 @@
+import com.kms.katalon.core.main.TestCaseMain
+import com.kms.katalon.core.logging.KeywordLogger
+import groovy.lang.MissingPropertyException
+import com.kms.katalon.core.testcase.TestCaseBinding
+import com.kms.katalon.core.driver.internal.DriverCleanerCollector
+import com.kms.katalon.core.model.FailureHandling
+import com.kms.katalon.core.configuration.RunConfiguration
+import com.kms.katalon.core.webui.contribution.WebUiDriverCleaner
+import com.kms.katalon.core.mobile.contribution.MobileDriverCleaner
+
+
+DriverCleanerCollector.getInstance().addDriverCleaner(new com.kms.katalon.core.webui.contribution.WebUiDriverCleaner())
+DriverCleanerCollector.getInstance().addDriverCleaner(new com.kms.katalon.core.mobile.contribution.MobileDriverCleaner())
+
+
+RunConfiguration.setExecutionSettingFile('C:\\Users\\THEFRA~1\\AppData\\Local\\Temp\\Katalon\\Test Cases\\Push all the buttons\\20180329_145327\\execution.properties')
+
+TestCaseMain.beforeStart()
+try {
+    
+	    TestCaseMain.runTestCase('Test Cases/Push all the buttons', new TestCaseBinding('Test Cases/Push all the buttons', [:]), FailureHandling.STOP_ON_FAILURE )
+    
+} catch (Exception e) {
+    TestCaseMain.logError(e, 'Test Cases/Push all the buttons')
+}
--- GM-US_Lead_Gen_Reporting_Tool/Test Suites/GM- Reporting tool suite.ts
+++ GM-US_Lead_Gen_Reporting_Tool/Test Suites/GM- Reporting tool suite.ts
@@ -4,10 +4,41 @@
    <name>GM- Reporting tool suite</name>
    <tag></tag>
    <isRerun>false</isRerun>
+   <lastRun>2018-03-29T15:16:48</lastRun>
    <mailRecipient>jwaryas@bluewatertech.com;</mailRecipient>
    <numberOfRerun>0</numberOfRerun>
    <pageLoadTimeout>30</pageLoadTimeout>
    <pageLoadTimeoutDefault>true</pageLoadTimeoutDefault>
    <rerunFailedTestCasesOnly>false</rerunFailedTestCasesOnly>
    <testSuiteGuid>970114b9-bf42-442e-ae72-7660b0a0e74a</testSuiteGuid>
+   <testCaseLink>
+      <guid>fb62be14-bd9c-434d-8558-fd2063ef7903</guid>
+      <isReuseDriver>false</isReuseDriver>
+      <isRun>true</isRun>
+      <testCaseId>Test Cases/Browser Start</testCaseId>
+   </testCaseLink>
+   <testCaseLink>
+      <guid>d649dba9-5626-43e0-8db1-f9939708d8f6</guid>
+      <isReuseDriver>false</isReuseDriver>
+      <isRun>true</isRun>
+      <testCaseId>Test Cases/Logging In</testCaseId>
+   </testCaseLink>
+   <testCaseLink>
+      <guid>4c073221-077f-40e6-9a28-6e53fac7ce2e</guid>
+      <isReuseDriver>false</isReuseDriver>
+      <isRun>true</isRun>
+      <testCaseId>Test Cases/Push all the buttons</testCaseId>
+   </testCaseLink>
+   <testCaseLink>
+      <guid>25dd2e3f-1c55-4328-8f5f-d6d3bd6110c1</guid>
+      <isReuseDriver>false</isReuseDriver>
+      <isRun>true</isRun>
+      <testCaseId>Test Cases/Logging Out</testCaseId>
+   </testCaseLink>
+   <testCaseLink>
+      <guid>c85e4a96-1a4f-4f29-b222-d6e78a856729</guid>
+      <isReuseDriver>false</isReuseDriver>
+      <isRun>true</isRun>
+      <testCaseId>Test Cases/Regression - Logging in and Logging out</testCaseId>
+   </testCaseLink>
 </TestSuiteEntity>
